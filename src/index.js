b5b541376a11a296250922e30510a8de33bb2847
Mikael Lirbank <mikael@airlab.io>
2017-03-18 21:42:08 -0700

    Temporarily enable use of local user/pass file

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import fs from 'fs';
 import path from 'path';
 import os from 'os';
+import request from 'request-promise-native';
 
 const file = fs.readFileSync(path.resolve(os.homedir(), 'habibi'));
-const [username, password] = file.toString().trim().split('\n').map(e => e.trim());
+const [username, password] = file.toString().trim().split('\n').map(
+  e => e.trim());
 
-console.log(username);
-console.log(password);
+const options = {
+  uri: 'http://localhost:3000/',
+  headers: {
+    username: username,
+    password: password,
+  },
+};
+
+request(options).then((html) => {
+  console.log(html);
+});
