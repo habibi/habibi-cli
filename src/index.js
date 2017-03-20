f891085a313edbf23fda8cfc4c3a382a98b22452
Mikael Lirbank <mikael@airlab.io>
2017-03-19 22:49:02 -0700

    Make linkable command line program

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import path from 'path';
 import os from 'os';
 import request from 'request-promise-native';
 
+const [, , command = ''] = process.argv;
+
+if (command.toLowerCase() !== 'push' && command.toLowerCase() !== 'pull') {
+  // eslint-disable-next-line no-console
+  console.error('Usage: habibi push|pull');
+  process.exit(1);
+}
+
 const file = fs.readFileSync(path.resolve(os.homedir(), 'habibi'));
 const [username, password] = file.toString().trim().split('\n').map(
   e => e.trim());
